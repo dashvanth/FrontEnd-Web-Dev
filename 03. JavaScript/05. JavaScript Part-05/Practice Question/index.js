eb71312f961dd5706211893649a5c383e21d2e1f
Dashvanth <dashvanthrajhc@gmail.com>
2023-07-25 22:43:34 +0530

    JavaScript: JavaScript Part 5

---
+++ 03. JavaScript/05. JavaScript Part-05/Practice Question/index.js
@@ -0,0 +1,18 @@
+const dice=Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+const car={
+    Name:"maruthi",
+    Model:800,
+    color:"silver"
+};
+console.log(car.Name);
+
+const person={
+    Name:"dashvanth",
+    age:22,
+    city:"Bengaluru"
+};
+person.city="Ramanagar";
+person.country="india";
+console.log(person);
